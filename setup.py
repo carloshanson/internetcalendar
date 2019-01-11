382b05bc39dea024973912708a2cdcfb8f8f27b6
Carlos Hanson <chanson@ttsd.k12.or.us>
2019-01-11 14:04:32 -0800

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='internetcalendar',
+        version='0.1',
+        description='Parse iCalendar events.',
+        url='https://github.com/carloshanson/internetcalendar',
+        author='Carlos Hanson',
+        author_email='carlos@zarega.com',
+        license='MIT',
+        packages=['internetcalendar'],
+        install_requires=[
+            ],
+        zip_safe=False)
+
